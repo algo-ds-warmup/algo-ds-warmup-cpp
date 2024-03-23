7efa8a1f77ea7b95f4e43add4c2ca87f0bc4a3f2
Uladzislau Yukhno <uladzislau.yukhno@gmail.com>
2024-03-23 15:43:22 +0000

    Add reveal cards in increasing order solution

--- sprint4/reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
+++ sprint4/reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
@@ -1,6 +1,8 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest/doctest.h"
+#include "fmt/core.h"
 #include "fmt/ranges.h"
+#include <deque>
 
 namespace doctest
 {
@@ -11,68 +13,31 @@ struct StringMaker<std::vector<T>>
 };
 } // namespace doctest
 
-std::vector<int> revealAlg(std::vector<int> v)
-{
-    std::vector<int> res{};
-    while (!v.empty())
-    {
-        auto& card = v.front();
-        res.push_back(card);
-        v.erase(v.begin());
-        if (v.size() > 1)
-        {
-            v.push_back(v.front());
-            v.erase(v.begin());
-        }
-    }
-
-    return res;
-}
-
 std::vector<int> deckRevealedIncreasing(std::vector<int>& deck)
 {
     std::ranges::sort(deck);
-    return revealAlg(deck);
-}
+    if (deck.size() <= 2)
+        return deck;
 
-//              [7, 13, 11, 17]
-// reveal 7 ->  [11, 17, 13]
-// reveal 11 -> [13, 17]
-// reveal 13 -> [17]
-// reveal 17;
+    std::deque<int> res;
+    auto rIt = deck.rbegin();
+    res.push_front(*rIt);
 
-// [7, 11, 13, 17] -> [7, 13, 11, 17]
-// [7] -> [13, 17, 11]
-// [7, 13] -> [11, 17]
-// [7, 13, 11, 17]
+    rIt++;
+    res.push_front(*rIt);
 
-TEST_CASE("Example 1")
-{
-    auto in = std::vector<int>{11, 17, 13};
-    REQUIRE(std::ranges::is_sorted(revealAlg(deckRevealedIncreasing(in))));
-}
+    while (++rIt != deck.rend())
+    {
+        res.push_front(res.back());
+        res.pop_back();
+        res.push_front(*rIt);
+    }
 
-TEST_CASE("Example 2")
-{
-    auto in = std::vector<int>{1, 1000};
-    REQUIRE(std::ranges::is_sorted(revealAlg(deckRevealedIncreasing(in))));
+    return {res.begin(), res.end()};
 }
 
 TEST_CASE("Example 3")
 {
     auto in = std::vector<int>{17, 13, 11, 2, 3, 5, 7};
-    //                                                      [2, 3, 5, 7, 11, 13, 17]
     REQUIRE((deckRevealedIncreasing(in)) == std::vector<int>{2, 13, 3, 11, 5, 17, 7});
-
-//     [2, 13, 3, 11, 5, 17, 7]
-    // [2] -> [3, 11, 5, 17, 7, 13]
-    // [2, 3] ->     [5, 17, 7, 13, 11]
-    // [2, 3, 5] ->          7, 13, 11, 17]
-    // [2, 3, 5, 7 ] ->             11, 17, 13]
-    // [2, 3, 5, 7, 11] ->                  13, 17]
-    // [2, 3, 5, 7, 11, 13] ->                  17]
-    // [2, 3, 5, 7, 11, 13, 17] ->                ]
 }
-
-// [11, 13, 17]
-// [11, ]
